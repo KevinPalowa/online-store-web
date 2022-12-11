1a70aaa2f40ea5b9a1d8625ea37690de2fbbe55e
Kevin Palowa <42116453+KevinPalowa@users.noreply.github.com>
2022-12-11 18:50:30 +0700

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -21,7 +21,7 @@ export default function Card({ src, title, children, price }: Props) {
         <p className="text-gray-700 text-base mb-4 font-bold">Rp. {price}</p>
         <Link
           className=" inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          href={`https://wa.me/+6283836222168?text=Saya+mau+pesan+${title}`}
+          href={`https://wa.me/6282361558036?text=Saya+mau+pesan+${title} Rp. ${price}`}
           target="_blank"
         >
           Chat Whatsapp
