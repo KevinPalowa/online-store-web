b401d15cf49ce48a3bb3420120addb261907dd06
Kevin Palowa <42116453+KevinPalowa@users.noreply.github.com>
2022-12-11 16:53:03 +0700

    Update Jumbotron.tsx

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -1,7 +1,7 @@
 export default function Jumbotron() {
   return (
     <div className="w-full bg-gray-900 py-36 space-y-3 flex flex-col items-center">
-      <h1 className="text-white text-5xl font-bold">Shop in style</h1>
+      <h1 className="text-white text-5xl font-bold">Selamat datang di gardam olshop</h1>
       <p className="text-gray-600 ">With this shop hompeage template</p>
     </div>
   );
