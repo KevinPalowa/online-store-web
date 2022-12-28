8ffd1b5611a3d1efad15f3ffe824ad787e4e76e1
Kevin Palowa <kevinpalowa@gmail.com>
2022-12-28 20:24:41 +0700

    update

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -1,16 +1,25 @@
+import Image from "next/image";
+
 export default function Jumbotron() {
   return (
     <div className="w-full bg-gray-900 py-36 space-y-3 flex flex-col items-center">
-      <h1 className="text-white text-5xl font-bold">Gardam olshop</h1>
+      {/* <h1 className="text-white text-5xl font-bold">Gardam olshop</h1> */}
       <div className="text-gray-500 flex">
-        <p>Kami menjual :</p>
-        <ul>
-          <li>cemilan medan</li>
-          <li>Pakaian perempuan</li>
-          <li>Rempah karo</li>
-          <li>Madu</li>
-          <li>Skincare</li>
-        </ul>
+        <Image
+          src="/img/jumbotron.jpeg"
+          alt="jumbotron image"
+          width={300}
+          height={300}
+          className="object-cover"
+        />
+        {/* <p>Kami menjual :</p> */}
+        {/* <ul> */}
+        {/*   <li>cemilan medan</li> */}
+        {/*   <li>Pakaian perempuan</li> */}
+        {/*   <li>Rempah karo</li> */}
+        {/*   <li>Madu</li> */}
+        {/*   <li>Skincare</li> */}
+        {/* </ul> */}
       </div>
     </div>
   );
